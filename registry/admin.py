692c5d141e98e01980e6d26603400bca6a0af70f
Abderrahhmane Metnani <abdulrahman.metnani@gmail.com>
2025-10-29 14:33:05 +0400

    Testing code

--- registry/admin.py
+++ registry/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Authorization, Activity, Operator, Contact, Aircraft
+from .models import Authorization, Activity, Operator, Contact, Aircraft, Pilot
 # Register your models here.
 
 admin.site.register(Authorization)
@@ -7,3 +7,4 @@ admin.site.register(Activity)
 admin.site.register(Operator)
 admin.site.register(Contact)
 admin.site.register(Aircraft)
+admin.site.register(Pilot)
